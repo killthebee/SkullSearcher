8f0ce808f0db8eb68a8f55a1a6ee1d0f4a856d35
killthebee <danny.developer@ro.ru>
2024-03-14 15:31:37 +0300

    add sf pro display fonts

--- SkullSearcher/Views/LoginViewController.swift
+++ SkullSearcher/Views/LoginViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let textLable: UILabel = {
+        let lable = UILabel()
+        lable.font = UIFont(name: "SFProDisplay-Semibold", size: 40)
+        lable.text = " test "
+        
+        return lable
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
+        view.addSubview(textLable)
+        textLable.frame = view.bounds
     }
 }
