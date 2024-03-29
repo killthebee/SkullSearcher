3e1e53f57bfe361bc00308849e3ed45bfd2e5549
killthebee <danny.developer@ro.ru>
2024-03-29 08:05:52 +0300

    organised app coordinator

--- SkullSearcher/Coordinators/AppCoordinator.swift
+++ SkullSearcher/Coordinators/AppCoordinator.swift
@@ -1,34 +1,49 @@
 import UIKit
 
 final class AppCoordinator: Coordinator {
-    
+
     private var window: UIWindow?
-    
+
     init(window: UIWindow) {
         self.window = window
     }
-    
-    var loginCoordinator : MainScreenCoordinator?
-//    var loginCoordinator : LoginCoordinator?
-    
+
+    var loginCoordinator : LoginCoordinator?
+
     @discardableResult
     func start() -> Presentable? {
-//        loginCoordinator = LoginCoordinator()
-//        let loginVC = loginCoordinator?.start()
-//        self.window?.rootViewController = loginVC as? LoginViewController
-//        self.window?.makeKeyAndVisible()
-//
-//        return loginVC
-        loginCoordinator = MainScreenCoordinator()
-        loginCoordinator?.apiService = MockApiService.shared
-        let loginVC = loginCoordinator?.start() as? MainViewController
-        let viewModel = MainViewModel()
-        viewModel.apiService = loginCoordinator?.apiService
-        viewModel.coordinator = loginCoordinator
-        loginVC?.viewModel = viewModel
-        self.window?.rootViewController = loginVC
+        loginCoordinator = LoginCoordinator()
+        let loginVC = loginCoordinator?.start()
+        self.window?.rootViewController = loginVC as? LoginViewController
         self.window?.makeKeyAndVisible()
 
         return loginVC
     }
 }
+
+// MARK: - Fast trek to main screen
+//final class AppCoordinator: Coordinator {
+//
+//    private var window: UIWindow?
+//
+//    init(window: UIWindow) {
+//        self.window = window
+//    }
+//
+//    var loginCoordinator : MainScreenCoordinator?
+//
+//    @discardableResult
+//    func start() -> Presentable? {
+//        loginCoordinator = MainScreenCoordinator()
+//        loginCoordinator?.apiService = MockApiService.shared
+//        let loginVC = loginCoordinator?.start() as? MainViewController
+//        let viewModel = MainViewModel()
+//        viewModel.apiService = loginCoordinator?.apiService
+//        viewModel.coordinator = loginCoordinator
+//        loginVC?.viewModel = viewModel
+//        self.window?.rootViewController = loginVC
+//        self.window?.makeKeyAndVisible()
+//
+//        return loginVC
+//    }
+//}
